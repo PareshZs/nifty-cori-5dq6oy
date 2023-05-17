d0576e87931201c9a3616e2b081194ed5fda117c
Paresh zambre <113985139+PareshZs@users.noreply.github.com>
2023-05-17 07:06:22 +0000

    Upadtes centeredtree and removed text overlapping

--- src/CenteredTree.js
+++ src/CenteredTree.js
@@ -2,62 +2,161 @@ import React from "react";
 import Tree from "react-d3-tree";
 import { viewData } from "./data";
 
+// const tableData = [
+//   {
+//     name: "Table",
+//     children: [
+//       {
+//         name: "Dev - Turbot zza",
+//         children: [
+//           {
+//             name: "FullAWS",
+//           },
+//         ],
+//       },
+//       {
+//         name: "Dev - Cloud Management zzb",
+//         children: [
+//           {
+//             name: "FullAWS",
+//           },
+//           {
+//             name: "Restrict",
+//           },
+//           {
+//             name: "FullAWS",
+//           },
+//         ],
+//       },
+//       {
+//         name: "Prod - AWS Organizations (Secondary) zze",
+//         children: [
+//           {
+//             name: "FullAWS",
+//           },
+//         ],
+//       },
+//       {
+//         name: "Dev - testing app zzh",
+//         children: [
+//           {
+//             name: "FullAWS",
+//           },
+//         ],
+//       },
+//       {
+//         name: "Dev - Cloud Security Monitoring zzk",
+//         children: [
+//           {
+//             name: "FullAWS",
+//           },
+//         ],
+//       },
+//       {
+//         name: "EC-Default Dev Collab zzg",
+//         children: [
+//           {
+//             name: "FullAWS",
+//           },
+//         ],
+//       },
+//     ],
+//   },
+// ];
+
 const tableData = [
   {
-    name: "Table",
+    name: "DIM.DIAGNOSIS",
     children: [
       {
-        name: "Dev - Turbot zza",
+        name: "DIM.DIAGNOSIS",
         children: [
-          {
-            name: "FullAWS",
-          },
+          { name: "SNXS.PUB_PROGRAM_PRODUCT" },
+          { name: "MCKN.PUB_TXO_ADC_CLAIMS" },
+          { name: "IION.PUB_COMM_DRUG_INV" },
+          { name: "IION.PUB_TV_ALERTS" },
+          { name: "MCKN.PUB_EMR_DATA" },
+          { name: "MCKN.PUB_TUC_CLAIMS_ALERT" },
+          { name: "NTHR.PUB_CLAIMS_DATA" },
+          { name: "IION.PUB_TUC_ALERTS" },
+          { name: "DIM.DIAGNOSIS" },
         ],
       },
       {
-        name: "Dev - Cloud Management zzb",
+        name: "SNXS.PUB_PROGRAM_PRODUCT",
+        children: [{ name: "SNXS.PUB_PROGRAM_PRODUCT" }],
+      },
+      {
+        name: "IION.PUB_TV_ALERTS",
         children: [
-          {
-            name: "FullAWS",
-          },
-          {
-            name: "Restrict",
-          },
-          {
-            name: "FullAWS",
-          },
+          { name: "IION.PUB_TV_ALERTS" },
+          { name: "IION.POSTDQM_TV_ALERTS" },
         ],
       },
       {
-        name: "Prod - AWS Organizations (Secondary) zze",
+        name: "IION.POSTDQM_TV_ALERTS",
         children: [
-          {
-            name: "FullAWS",
-          },
+          { name: "IION.POSTDQM_TV_ALERTS" },
+          { name: "IION.VW_TV_ALERTS" },
         ],
       },
       {
-        name: "Dev - testing app zzh",
+        name: "MCKN.PUB_EMR_DATA",
         children: [
-          {
-            name: "FullAWS",
-          },
+          { name: "MCKN.PUB_EMR_DATA" },
+          { name: "MCKN.POSTDQM_EMR_DATA" },
         ],
       },
       {
-        name: "Dev - Cloud Security Monitoring zzk",
+        name: "MCKN.POSTDQM_EMR_DATA",
         children: [
-          {
-            name: "FullAWS",
-          },
+          { name: "MCKN.PUB_EMR_DATA" },
+          { name: "MCKN.EMR_DATA" },
+          { name: "MCKN.POSTDQM_EMR_DATA" },
         ],
       },
       {
-        name: "EC-Default Dev Collab zzg",
+        name: "MCKN.PUB_TUC_CLAIMS_ALERT",
         children: [
-          {
-            name: "FullAWS",
-          },
+          { name: "MCKN.PUB_TUC_CLAIMS_ALERT" },
+          { name: "MCKN.POSTDQM_TUC_CLAIMS_ALERT" },
+        ],
+      },
+      {
+        name: "MCKN.POSTDQM_TUC_CLAIMS_ALERT",
+        children: [
+          { name: "MCKN.PUB_TUC_CLAIMS_ALERT" },
+          { name: "MCKN.TUC_CLAIMS_ALERT" },
+          { name: "MCKN.POSTDQM_TUC_CLAIMS_ALERT" },
+        ],
+      },
+      {
+        name: "NTHR.PUB_CLAIMS_DATA",
+        children: [
+          { name: "NTHR.PUB_CLAIMS_DATA" },
+          { name: "NTHR.POSTDQM_CLAIMS_DATA" },
+        ],
+      },
+      {
+        name: "NTHR.POSTDQM_CLAIMS_DATA",
+        children: [
+          { name: "NTHR.CLAIMS_DATA" },
+          { name: "NTHR.PUB_CLAIMS_DATA" },
+          { name: "NTHR.POSTDQM_CLAIMS_DATA" },
+        ],
+      },
+      {
+        name: "IION.PUB_TUC_ALERTS",
+        children: [
+          { name: "IION.PUB_TUC_ALERTS" },
+          { name: "IION.POSTDQM_TUC_ALERTS" },
+        ],
+      },
+      {
+        name: "IION.POSTDQM_TUC_ALERTS",
+        children: [
+          { name: "IION.TUC_ALERTS" },
+          { name: "IION.POSTDQM_TUC_ALERTS" },
         ],
       },
     ],
@@ -69,8 +168,30 @@ const containerStyles = {
   height: "100vh",
 };
 
+const renderForeignObjectNode = ({
+  nodeDatum,
+  toggleNode,
+  foreignObjectProps,
+}) => (
+  <g>
+    <circle r={15}></circle>
+    {/* `foreignObject` requires width & height to be explicitly set. */}
+    <foreignObject {...foreignObjectProps}>
+      <div style={{ border: "1px solid black", backgroundColor: "#dedede" }}>
+        <h3 style={{ textAlign: "center" }}>{nodeDatum.name}</h3>
+        {nodeDatum.children && (
+          <button style={{ width: "100%" }} onClick={toggleNode}>
+            {nodeDatum.__rd3t.collapsed ? "Expand" : "Collapse"}
+          </button>
+        )}
+      </div>
+    </foreignObject>
+  </g>
+);
+
 export default class CenteredTree extends React.PureComponent {
   state = {};
+  nodeSize = { x: 300, y: 200 };
 
   componentDidMount() {
     const dimensions = this.treeContainer.getBoundingClientRect();
@@ -89,12 +210,19 @@ export default class CenteredTree extends React.PureComponent {
           data={
             this?.props?.optionValue[0]?.label === "View" ? viewData : tableData
           }
-          onLinkMouseOver={() => {
-            console.log("onLinkMouseOver");
+          onLinkMouseOver={(tree) => {
+            console.log("onLinkMouseOver", tree);
           }}
-          onLinkClick={() => {
-            console.log("onLinkClick");
+          onLinkClick={(link) => {
+            console.log("onLinkClick", link);
           }}
+          onNodeClick={(node) => {
+            console.log("onNodeClick", node);
+          }}
+          onNodeMouseOver={(node) => {
+            console.log("onNodeClick", node);
+          }}
+          nodeSize={this.nodeSize}
           translate={this.state.translate}
           orientation={"vertical"}
         />
--- src/apiService.js
+++ src/apiService.js
@@ -1,5 +1,5 @@
-import axios from "axios";
 import React from "react";
+import axios from "axios";
 
 const client = axios.create({
   baseURL: "https://jsonplaceholder.typicode.com/posts",
